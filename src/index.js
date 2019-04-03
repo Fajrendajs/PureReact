8575fe67db7393f85b803374a70011e897048027
Fajrendajs <40194093+Fajrendajs@users.noreply.github.com>
2019-04-03 13:42:14 +0200

    juicy

--- src/index.js
+++ src/index.js
@@ -3,14 +3,9 @@ import ReactDOM from 'react-dom';
 
 import './styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+function HelloWorld(props) {
+  return <span>{props.song.name}</span>;
 }
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<HelloWorld song={{ name: 'Juicy' }} />, rootElement);
